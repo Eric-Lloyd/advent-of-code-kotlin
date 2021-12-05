0641c2c4d91b63833b41fdca3655115351dc14eb
Eric-Lloyd <ericlloyd19@gmail.com>
2021-12-05 10:41:46 +0100

    Solution: Day5

---
+++ src/day5/Day5.kt
@@ -0,0 +1,96 @@
+package day5
+
+import readInput
+import kotlin.math.abs
+
+fun main() {
+    val lines = readInput("day5/Day5")
+        .map { lineFrom(it) }
+    val count1 = count(lines) { line -> line.isVertical || line.isHorizontal }
+    val count2 = count(lines) { line -> line.isVertical || line.isHorizontal || line.isDiagonal }
+    println(count1)
+    println(count2)
+}
+
+fun count(lines: List<Line>, predicate: (Line) -> Boolean): Int {
+    val filteredLines = lines.filter { predicate(it) }
+    return overlappingPoints(filteredLines, 2)
+}
+
+fun overlappingPoints(lines: List<Line>, threshold: Int) : Int {
+    val counts = mutableMapOf<Point, Int>()
+    for (line in lines) {
+        val points = line.points()
+        for (point in points) {
+            if (counts.containsKey(point)) {
+                counts[point] = counts[point]!! + 1
+            } else {
+                counts[point] = 1
+            }
+        }
+    }
+    return counts.count { (_, count) -> count >= threshold }
+}
+data class Point(val x: Int, val y: Int)
+
+data class Line(val from: Point, val to: Point) {
+    val isHorizontal : Boolean = from.y == to.y
+    val isVertical : Boolean = from.x == to.x
+    val isDiagonal : Boolean = abs(from.x - to.x) == abs(from.y - to.y)
+
+    /*
+     * returns all the points along the line.
+     */
+    fun points(): List<Point> {
+        val points = mutableListOf<Point>()
+        when {
+            isHorizontal -> {
+                val min = minOf(from.x, to.x)
+                val max = maxOf(from.x, to.x)
+                for (i in min until max + 1) {
+                    points.add(Point(i, from.y))
+                }
+                return points
+            }
+            isVertical -> {
+                val min = minOf(from.y, to.y)
+                val max = maxOf(from.y, to.y)
+                for (i in min until max + 1) {
+                    points.add(Point(from.x, i))
+                }
+                return points
+            }
+            isDiagonal -> {
+                points.add(from)
+                val xDirection = direction(from.x, to.x) // whether to increment or decrement x when traversing the line
+                val yDirection = direction(from.y, to.y) // whether to increment or decrement y when traversing the line
+                var currentX = xDirection(from.x)
+                var currentY = yDirection(from.y)
+                while (currentX != to.x) {
+                    points.add(Point(currentX, currentY))
+                    currentX = xDirection(currentX)
+                    currentY = yDirection(currentY)
+                }
+                points.add(to)
+                return points
+            }
+            else -> {
+                throw NotImplementedError("We only support horizontal, vertical or diagonal lines.")
+            }
+        }
+    }
+}
+
+fun direction(a1: Int, a2: Int) : (Int) -> Int {
+    if (a1 > a2) return { a -> a - 1 } else return { a -> a + 1 }
+}
+
+fun lineFrom(line: String): Line {
+    val (from, to) = line
+        .split(" -> ")
+        .map { subLine ->
+            val (x, y) = subLine.split(",").map { it.toInt() }
+            Point(x, y)
+        }
+    return Line(from, to)
+}

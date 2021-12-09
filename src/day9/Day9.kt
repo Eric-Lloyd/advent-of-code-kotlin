7c1389b304bb882a4182c342e2071bd878ad17f0
Eric-Lloyd <ericlloyd19@gmail.com>
2021-12-09 11:24:32 +0100

    fix typos

--- src/day9/Day9.kt
+++ src/day9/Day9.kt
@@ -3,10 +3,12 @@ package day9
 import readInput
 import java.util.*
 
+const val MAX_VALUE = 9
+
 fun main() {
     val input = readInput("day9/Day9")
         .map { line -> line.split("").mapNotNull { runCatching { it.toInt() }.getOrNull() } }
-    val lowPointSum = findLowPoint(input).sumOf { it + 1 }
+    val lowPointSum = findLowPoints(input).sumOf { it + 1 }
     println(lowPointSum)
     val largerBasinsProduct = findBasins(input)
         .sortedBy { it.size }
@@ -15,17 +17,17 @@ fun main() {
     println(largerBasinsProduct)
 }
 
-fun findLowPoint(matrix: List<List<Int>>): List<Int> {
+fun findLowPoints(matrix: List<List<Int>>): List<Int> {
     val result = mutableListOf<Int>()
     val n = matrix[0].size
     val m = matrix.size
     for (j in 0 until m) {
         for (i in 0 until n) {
             val curr = matrix[j][i]
-            val left = if (i > 0) matrix[j][i - 1] else Int.MAX_VALUE
-            val right = if (i < n - 1) matrix[j][i + 1] else Int.MAX_VALUE
-            val top = if (j > 0) matrix[j - 1][i] else Int.MAX_VALUE
-            val bottom = if (j < m - 1) matrix[j + 1][i] else Int.MAX_VALUE
+            val left = if (i > 0) matrix[j][i - 1] else MAX_VALUE
+            val right = if (i < n - 1) matrix[j][i + 1] else MAX_VALUE
+            val top = if (j > 0) matrix[j - 1][i] else MAX_VALUE
+            val bottom = if (j < m - 1) matrix[j + 1][i] else MAX_VALUE
             if (curr < left && curr < right && curr < top && curr < bottom) {
                 result.add(curr)
             }
@@ -83,9 +85,6 @@ fun findBasins(matrix: List<List<Int>>): List<Set<BasinSpot>> {
     }
     return basins
 }
-
-const val MAX_VALUE = 9
-
 data class BasinSpot(val value: Int, val i: Int, val j: Int, var handled: Boolean = false) {
     // ignore 'handled' in hashcode
     override fun hashCode(): Int {

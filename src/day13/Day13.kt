e3feae52dde9b3ca43dc692d545fc18d058ed415
Eric-Lloyd <ericlloyd19@gmail.com>
2021-12-13 16:43:55 +0100

    small refactoring

--- src/day13/Day13.kt
+++ src/day13/Day13.kt
@@ -7,30 +7,33 @@ fun main() {
     val emptyLineIndex = input.indexOfFirst { it.isBlank() }
 
     val coordinates = input.subList(0, emptyLineIndex)
-        .map { line -> line.split(",").map { it.toInt() }.let { (x, y) -> Pair(x, y) } }
+        .map { it.toCoordinate() }
 
     val folds = input.subList(emptyLineIndex + 1, input.size)
-        .map { line -> line.replace("fold along ", "").split("=").let { (axis, value) -> Pair(axisFrom(axis), value.toInt()) } }
+        .map { it.toFold() }
 
     folds.fold(buildInitialMatrix(coordinates)) { acc, (axis, value) ->
         acc.foldOn(axis, value).also { println(it.count()) }
     }.prettyPrint()
 }
 
+private fun String.toCoordinate() = this
+    .split(",")
+    .map { it.toInt() }
+    .let { (x, y) -> Pair(x, y) }
+
+private fun String.toFold() = this
+    .replace("fold along ", "")
+    .split("=")
+    .let { (axis, value) -> Pair(Axis.from(axis), value.toInt()) }
+
 fun buildInitialMatrix(coordinates: List<Pair<Int, Int>>): List<List<Boolean>> {
-    val result = mutableListOf<MutableList<Boolean>>()
     val (n, m) = coordinates
         .fold(Pair(0, 0)) { (currMaxX, currMaxY), (x, y) -> Pair(maxOf(currMaxX, x), maxOf(currMaxY, y)) }
         .let { (maxX, maxY) -> Pair(maxX + 1, maxY + 1) }
 
     // initialize boolean matrix to false
-    for (j in 0 until m) {
-        val row = mutableListOf<Boolean>()
-        for (i in 0 until n) {
-            row.add(false)
-        }
-        result.add(row)
-    }
+    val result = MutableList(m) { MutableList(n) { false } }
 
     // fill with coordinates
     for ((x, y) in coordinates) {
@@ -44,52 +47,49 @@ fun List<List<Boolean>>.foldOn(axis: Axis, value: Int): List<List<Boolean>> {
     val m = this.size
     return when (axis) {
         Axis.Y -> {
-            val copy = this.take(value).toMutableList()
+            val folded = this.take(value).toMutableList()
             var downIndex = value - 1
             var upIndex = value + 1
             while (downIndex >= 0 && upIndex < m) {
                 val downRow = this[downIndex]
                 val upRow = this[upIndex]
-                val merge = downRow.mapIndexed { index, b -> b || upRow[index] }
-                copy[downIndex] = merge
+                folded[downIndex] = downRow.merge(upRow)
                 downIndex -= 1
                 upIndex += 1
             }
-            copy
+            folded
         }
         Axis.X -> {
-            val copy = this.map { it.take(value).toMutableList() }
+            val folded = this.map { it.take(value).toMutableList() }
             var downIndex = value - 1
             var upIndex = value + 1
             while (downIndex >= 0 && upIndex < n) {
                 val downRow = this.map { it[downIndex] }
                 val upRow = this.map { it[upIndex] }
-                val merge = downRow.mapIndexed { index, b -> b || upRow[index] }
+                val merge = downRow.merge(upRow)
                 for (j in 0 until m) {
-                    copy[j][downIndex] = merge[j]
+                    folded[j][downIndex] = merge[j]
                 }
                 downIndex -= 1
                 upIndex += 1
             }
-            copy
+            folded
         }
     }
 }
 
-fun List<List<Boolean>>.count() = this.flatten().count { it }
+private fun List<Boolean>.merge(other: List<Boolean>) = this.mapIndexed { index, b -> b || other[index] }
 
-fun List<List<Boolean>>.prettyPrint() {
-    for (list in this) {
-        for (elem in list) {
-            if (elem) print("#") else print(".")
-        }
-        print("\n")
-    }
+private fun List<List<Boolean>>.count() = this.flatten().count { it }
+
+fun List<List<Boolean>>.prettyPrint() = this.forEach { line ->
+    line.forEach { elem -> if (elem) print("#") else print(" ") }.also { print("\n") }
 }
-
 
 enum class Axis {
-    X, Y
-}
+    X, Y;
 
-fun axisFrom(char: String) = if (char == "x") Axis.X else Axis.Y
+    companion object {
+        fun from(char: String) = if (char == "x") X else Y
+    }
+}
